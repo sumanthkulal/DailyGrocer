1a1c64a4ec917c1e31d0cd578091ae4e4bf8fff4
Sumanth Kulal <sumanthkulal2005@gmail.com>
2026-02-17 20:59:11 +0530

    implemented cart page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Login from './components/Login'
 import AllProducts from './pages/AllProducts'
 import ProductCatgory from './pages/ProductCatgory'
 import ProductDetails from './pages/ProductDetails'
+import Cart from './pages/Cart'
 
 const App = () => {
   const isSellerPath=useLocation().pathname.includes("seller")
@@ -26,6 +27,7 @@ const App = () => {
           <Route path='/products' element={<AllProducts/>}/>
           <Route path='/products/:category' element={<ProductCatgory/>}/>
           <Route path='/products/:category/:id' element={<ProductDetails/>}/>
+          <Route path='/cart' element={<Cart/>}/>
         </Routes>
           
       </div>
