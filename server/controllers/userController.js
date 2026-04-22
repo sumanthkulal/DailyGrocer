e75b603bb21d8e3a30243c8ace30a07310aa354c
Sumanth Kulal <sumanthkulal2005@gmail.com>
2026-04-22 22:39:37 +0530

    feat: implement login endpoint and token generation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 
+//register
 export const register = async  (req,res) =>{
     try{
             const {name , email ,password} =req.body;
@@ -38,4 +39,40 @@ export const register = async  (req,res) =>{
             console.log(error.message)
             res.json({success: false ,message:error.message})
     }
+}
+
+//login
+export const login = async (req,res) =>{
+    try{
+             const {email,password}= req.body;
+
+             if(!email || !password)
+                return res.json({success: false , message : 'Email and password are required'})
+
+             const user= await User.findOne({email})
+
+             if(!user)
+                return res.json({success: false , message : 'invalid Email and password'})
+
+             const isMatch= await bcrypt.compare(password,user.password);
+
+             if(!isMatch)
+                return res.json({success: false , message : 'invalid Email and password'})   
+
+              const token=jwt.sign({id: user._id}, process.env.JWT_SECRET,{expiresIn:'7d'})
+
+            res.cookie('token',token,{
+                httpOnly: true,
+                secure: process.env.NODE_ENV == 'production',
+                sameSite : process.env.NODE_ENV == 'production' ? 'none' : 'strict',
+                maxAge:7*24*60*1000,
+            })
+
+            return res.json({success:true ,user: {email:user.email,name:user.name}})
+
+    }catch(error){
+             console.log(error.message)
+            res.json({success: false ,message:error.message})
+    }
+    
 }
