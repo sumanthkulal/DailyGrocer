e14014da82c4e9099d72eebf1a294c9697274ec4
Sumanth Kulal <sumanthkulal2005@gmail.com>
2026-04-24 19:21:06 +0530

    feat: implement seller authentication system with JWT middleware and login/logout controllers

---
+++ server/middlewares/authSeller.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+
+const authSeller = async (req,res,next) =>{
+    const {sellerToken} =req.cookies;
+
+    if(!sellerToken)
+    {
+        return res.json({success: false ,message: 'Not Authorised'});
+    }
+
+    try{
+                const tokenDecode=jwt.verify(sellerToken,process.env.JWT_SECRET)
+                if(tokenDecode.email=== process.env.SELLER_EMAIL)
+                {
+                    next();
+                }else{
+                    return res.json({success:false,message:"Not authorised"});
+                }
+                
+        }catch(error)
+        {
+            res.json({success:false, message:error.message});
+        }
+}
+
+export  default authSeller;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import connectDB from './configs/db.js';
 import 'dotenv/config'
 import userRouter from './routes/userRoute.js';
+import sellerRouter from './routes/sellerRoute.js';
 
 const app=express()
 
@@ -17,8 +18,11 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({origin: allowedOrigins , credential: true}))
 
-app.use('/api/user',userRouter)
+
 app.get('/',(req,res)=> res.send("API is Working"))
+
+app.use('/api/user',userRouter)
+app.use('/api/seller',sellerRouter)
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`)
