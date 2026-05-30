ce961f26752738b1486e52900afbc3f62c95ade2
Sumanth Kulal <sumanthkulal2005@gmail.com>
2026-05-30 13:53:08 +0530

    updated frontend url

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 4000;
 await connectDB()
 await connectCloudinary();
 
-const allowedOrigins =['http://localhost:5173']
+const allowedOrigins =['http://localhost:5173','https://dailygrocer-orpin.vercel.app']
 app.post('/stripe',express.raw({type:'application/json'}),stripeWebHooks)
 
 app.use(express.json())
